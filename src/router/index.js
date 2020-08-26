1db28ee82d5106d40f52a8d882fce3122aff36ef
Yury Prymak <YuryPrymak@gmail.com>
2020-08-26 10:54:41 +0300

    Add pages, setup Vue Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Cart from '../views/Cart.vue';
+import Search from '../views/Search.vue';
+import Products from '../views/Products.vue';
+import Product from '../views/Product.vue';
 
 Vue.use(VueRouter);
 
@@ -8,22 +12,38 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
   },
+  {
+    path: '/search',
+    name: 'Search',
+    component: Search
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: Products
+  },
+  {
+    path: '/product',
+    name: 'Product',
+    component: Product
+  },
+  {
+    path: '*',
+    redirect: '/'
+  }
 ];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 export default router;
